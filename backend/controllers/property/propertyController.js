2db4aeedfd0764c852aa4e4fad039ac5bfec3748
Arjun <arjunsah856@gmail.com>
2026-04-02 22:49:40 +0545

    add, get all, get single and delete api

--- backend/controllers/property/propertyController.js
+++ backend/controllers/property/propertyController.js
@@ -1,6 +1,8 @@
 const Property = require("../../models/propertyModel")
 
 
+
+//add property --> by seller
 exports.addProperty = async(req, res) => {
     const ownerId = req.user.id
 
@@ -29,7 +31,89 @@ exports.addProperty = async(req, res) => {
     })
 
     res.status(200).json({
+        success: true,
         message: "Property add successfully.",
         data: property
+    })
+}
+
+
+
+
+//get all property  --> by all users
+exports.getProperties = async (req, res) => {
+    const property = await Property.find()
+    if(property.length == 0){
+        res.status(400).json({
+            message: "No property found",
+            property: []        
+        })
+    } else {
+        res.status(200).json({
+            message: "Property found",
+            property             
+        })
+    }
+}
+
+
+
+
+//get property by id  --> by all user
+exports.getProperty = async (req, res) => {
+    const {id} = req.params
+    if(!id) {
+        return res.status(400).json({
+            message: "Please provide Property id",
+        })
+    }
+
+    const property = await Property.find({ _id : id })      
+    if(property.length == 0){
+        res.status(400).json({
+            message: "No Property found with that id",
+            property: [],       
+        })
+    } else {
+        res.status(200).json({
+            message: "Property found with that id",
+            Property,            
+        })
+    }
+}
+
+
+
+
+
+// delete property --> by seller/owner
+exports.deleteProperty = async (req, res) => {
+    const {id} = req.params  //propertyID
+
+    if(!id) {
+        return res.status(400).json({
+            message: "Please provide property id"
+        })
+    }
+
+    const property = await Property.findById(id)
+    if(!property) {
+        return res.status(404).json({
+            message: "No property found with that id"
+        })
+    }
+
+// console.log(property.owner.toString())
+// console.log(req.user.id)
+
+    if(property.owner.toString() !== req.user.id){
+        return res.status(401).json({
+            message: "you aren't owner of this property"
+        })
+    }
+
+    await Property.findByIdAndDelete(id)
+    res.status(200).json({
+        message: "Property deleted successfully"
     })
 }
--- backend/routes/propertyRoutes.js
+++ backend/routes/propertyRoutes.js
@@ -1,11 +1,20 @@
-const { addProperty } = require('../controllers/property/propertyController')
+const { addProperty, deleteProperty, getProperties, getProperty } = require('../controllers/property/propertyController')
+const catchAsync = require('../services/catchAsync')
 const isAuthenticated = require('./../middleware/isAuthenticated')
 const restrictTo = require('./../middleware/restrictTo')
 
 const router = require('express').Router()
 
 
-router.route('/add').post(isAuthenticated, restrictTo('seller'), addProperty)
+
+router.route('/')
+    .get(catchAsync(getProperties))
+    .post(isAuthenticated, restrictTo('seller'), catchAsync(addProperty))
+    
+
+router.route('/:id')
+    .get(catchAsync(getProperty))
+    .delete(isAuthenticated, restrictTo('seller'), catchAsync(deleteProperty))
 
 
 module.exports = router
