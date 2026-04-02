9f60f43f221a55f181b8751b34d3cc245b8c782c
Arjun <arjunsah856@gmail.com>
2026-04-02 09:31:28 +0545

    backend setup

---
+++ backend/server.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const dotenv = require('dotenv')
+
+
+const app = express()
+dotenv.config()
+
+
+//test api
+app.use('/', (req, res) => {
+    res.send("This is home page")
+})
+
+
+
+
+// LISTEN SERVER
+const PORT = process.env.PORT || 5000
+app.listen(PORT, ()=>{
+    console.log(`Server running at port ${PORT}`)
+})
