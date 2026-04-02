b86e9f592d4ebcc98d4e848e1db8a086f295b6d2
Arjun <arjunsah856@gmail.com>
2026-04-02 17:27:06 +0545

    login user api

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,14 +1,15 @@
 const User = require('./../models/userModel')
 const bcrypt = require("bcryptjs")
+const jwt = require('jsonwebtoken')
 
 
 //register user
 exports.registerUser = async(req, res) => {
-    const { name, email, password } = req.body
+    const { name, email, password, role } = req.body
 
-    if(!name || !email || !password){
+    if(!name || !email || !password || !role){
         return res.status(400).json({
-            message: "User name, email and password is required."
+            message: "User name, email, role and password is required."
         })
     }
 
@@ -25,10 +26,54 @@ exports.registerUser = async(req, res) => {
     await User.create({
         name,
         email,
-        password: hashedPw
+        password: hashedPw,
+        role
     })
 
-    return res.status(200).json({
+    res.status(200).json({
         message: "User registered successfully."
+    })
+}
+
+
+
+
+
+//login user
+exports.loginUser = async(req, res) => {
+    const { email, password } = req.body
+
+    if(!email || !password){
+        return res.status(400).json({
+            message: "User email and password is required"
+        })
+    }
+
+    // console.log(req.body)
+
+    const userFound = await User.findOne({email})
+    if(!userFound){
+        return res.status(404).json({
+            message: "User does not exists with this email."
+        })
+    }
+
+    // console.log(userFound)
+
+    const isMatch = bcrypt.compareSync(password, userFound.password)
+    if(!isMatch){
+        return res.status(400).json({
+            message: "Wrong password. Try again."
+        })
+    }
+
+    //generate token
+    const token = jwt.sign({id: userFound._id}, process.env.SECRET_KEY, {
+        expiresIn: '5h'
+    })
+    
+    res.status(200).json({
+        message: "Login successful.",
+        token
     })
 }
--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,10 +1,11 @@
-const { registerUser } = require('../controllers/authController')
+const { registerUser, loginUser } = require('../controllers/authController')
 const catchAsync = require('./../services/catchAsync')
 
 const router = require('express').Router()
 
 
 router.route('/register').post(catchAsync(registerUser))
+router.route('/login').post(catchAsync(loginUser))
 
 
 module.exports = router
