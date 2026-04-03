51669f2a5aed2576c487365b8e079d234abd4a38
Arjun <arjunsah856@gmail.com>
2026-04-03 14:37:45 +0545

    auth services, protectedRoutes

---
+++ frontend/src/services/authServices.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+const API = axios.create({
+  baseURL: "http://localhost:5000/api",
+});
+
+// Add token to headers automatically in every request
+API.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+  if (token) {
+    config.headers.Authorization = token;
+  }
+  return config;
+});
+
+export default API;
